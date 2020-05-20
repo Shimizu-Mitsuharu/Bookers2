5f64f24b0e542af6a9dfc0df19e3150785c4c8c7
vagrant <vagrant@localhost.localdomain>
2020-05-20 08:41:24 +0000

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,30 @@
 class UsersController < ApplicationController
 	def show
     @user = User.find(params[:id]) #追記
-end
-def index
+    end
+
+    def index
 	@users =User.all
 	@books = Book.all
 	@book = Book.new
 	@user = current_user
-end
+    end
 
+    def edit
+	@user = User.find(params[:id])
+	if @user.id != current_user.id
+		flash[:notice] = "can't successfully access!"
+		redirect_to user_path(current_user.id)
+	end
+	end
+
+	def update
+	@user = User.find(params[:id])
+	if @user.update(user_params)
+		flash[:notice] = "successfully update user!"
+		redirect_to user_path(@user.id)
+	else
+		render :edit
+	end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'layouts#home'
   get 'layouts/about' => 'home/about'
-  devise_for :users, controllers: {
-        registrations: 'users/registrations'
-}
+  devise_for :users
   resources :books
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
