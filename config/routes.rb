91d96e191f97520d7a0e29539b7c875f972d85f1
vagrant <vagrant@localhost.localdomain>
2020-05-21 03:07:06 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'layouts/about' => 'home/about'
   devise_for :users
   resources :books
-  resources :users
+  resources :users,only: [:show,:index,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
