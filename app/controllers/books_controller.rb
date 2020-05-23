9306c75b5a2f93ca9dec226a6aed6c4f9450f50a
vagrant <vagrant@localhost.localdomain>
2020-05-23 05:33:25 +0000

    [update]]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,21 @@ class BooksController < ApplicationController
  before_action :authenticate_user!
  before_action :ensure_correct_user, only: %i(edit update destroy)
 
- def show
+def show
   @book = Book.find(params[:id])
 end
 
 def create
- @book = Book.new(book_params)
- @book.user_id = current_user.id
- if @book.save
-  flash[:success] = "You have creatad book successfully."
-  redirect_to book_path(@book.id)
-else
-  flash.now[:danger] = @new_book.errors
-  render 'index'
-end
+  @new_book = Book.new(book_params)
+  @new_book.user_id = current_user.id
+  if @new_book.save
+    flash[:success] = "You have creatad book successfully."
+    redirect_to @new_book
+  else
+    @books = Book.all
+    flash.now[:danger] = @new_book.errors
+    render 'index'
+  end
 end
 
 def index
@@ -51,12 +52,4 @@ private
 
 def book_params
   params.require(:book).permit(:title, :body)
-end
-
-def ensure_correct_user
-  @book = Book.find(params[:id])
-  return if @book.user_id == current_user.id
-  flash[:danger] = '権限がありません'
-  redirect_to books_path
-end
 end
